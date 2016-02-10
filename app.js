9fa18f86fff4c5ba71a3b1856846e2bbb75d492e
MichaelMacLeod <edinburghmacleod@gmail.com>
2016-02-10 16:07:03 +0000

    Added mongoose to app.js

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ var path           = require('path');
 var bodyParser     = require('body-parser');
 var expressLayouts = require('express-ejs-layouts');
 var app            = express();
+var Animal = require('./models/animal');
+var Farm = require('./models/farm');
+//Mongoose: 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27019/farm-manager');
 
-//App settings
+//App settings: 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -16,6 +21,7 @@ app.use(expressLayouts);
 //Serve js & css files from a public folder
 app.use(express.static(__dirname + '/public'));
 
+//Let's add all the farms.locals here**********
 // ############ YOU CAN ADD YOUR ROUTES BELOW HERE
 
 
